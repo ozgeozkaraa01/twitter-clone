19f96d570bc21bbb1447b66f54562efb07ad4c4d
Ozgenur Ozkara <ozgenurozkara@gmail.com>
2022-09-28 11:55:19 +0300

    sign in page the completed

--- pages/auth/siginin.js
+++ pages/auth/siginin.js
@@ -2,25 +2,23 @@ import { getProviders, signIn } from "next-auth/react";
 
 export default function siginin({ providers }) {
   return (
-    <div flex>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png"
-        alt="twitter image"
-        className=""
-      />
-
-      <div className="">
-        {Object.values(providers).map((provider) => (
-          <div className="">
-            <img
-              src="https://cdn.cms-twdigitalassets.com/content/dam/help-twitter/en/twitter-tips/desktop-assets/ch-01/ch12findphone.png.twimg.1920.png"
-              alt=""
-            />
-
-            <button>Sign in with {provider.name}</button>
-          </div>
-        ))}
-      </div>
+    <div className="">
+      {Object.values(providers).map((provider) => (
+        <div className="flex mt-10 flex-col items-center">
+          <img
+            className="w-10 object-cover mb-5"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png"
+            alt=""
+          />
+          <h2 className="text-3xl font-bold mb-8">Login to Twitter</h2>
+          <button
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            className="bg-sky-200 rounded-lg p-3 text-white hover:bg-sky-400"
+          >
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
